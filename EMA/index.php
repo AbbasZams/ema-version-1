12defd168467c179b780f4ecf8f053150c00e1c8
Abbas Ahmad <Itsabbasahmad@gmail.com>
2017-09-16 04:06:37 -0700

    Ad

---
+++ EMA/index.php
@@ -0,0 +1,20 @@
+<?php
+# This function reads your DATABASE_URL configuration automatically set by Heroku
+# the return value is a string that will work with pg_connect
+function pg_connection_string() {
+  // we will fill this out next
+}
+ 
+# Establish db connection
+$db = pg_connect(pg_connection_string());
+if (!$db) {
+    echo "Database connection error."
+    exit;
+}
+ 
+$result = pg_query($db, "SELECT statement goes here");
+<<<<<<< HEAD
+?>
+=======
+?>
+>>>>>>> d9766fe9b7edeb28ff2dad818d53dfadd5f8447f
